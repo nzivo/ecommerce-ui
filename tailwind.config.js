edfc3f28a6776004b11e4f3fe344cb7cee07fe1f
nzivo <johnnnzivo@gmail.com>
2023-05-13 23:36:30 +0300

    Navbar Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1536px",
+    },
+    extend: {
+      colors: {
+        lightLuminousGreen: "#58e9a3",
+        luminousGreen: "#2fe48d",
+        darkLuminousGreen: "#25b670",
+        black: "#0D0D0D",
+        lightBlack: "#1F1F1F",
+        darkGrey: "#797979",
+        grey: "#E1E1E1",
+        lightGrey: "#EEEEEE",
+        white: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
-}
-
+};
