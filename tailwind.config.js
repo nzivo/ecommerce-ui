e0e49e7660ccc79950e10c7a936d9b283dd7152f
nzivo <johnnnzivo@gmail.com>
2023-05-14 01:07:40 +0300

    Sales Stats

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
       sm: "480px",
